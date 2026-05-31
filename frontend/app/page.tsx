fc8cda78ee33e73476b8a583a97cc113735737ad
Alexander Gerald <alex.gg.handoko@gmail.com>
2026-06-01 00:31:47 +0800

    Build basic working frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,65 +1,130 @@
-import Image from "next/image";
+"use client"
+
+import { Cormorant_Garamond } from "next/font/google"
+import { useState } from "react"
+
+const garamond = Cormorant_Garamond()
 
 export default function Home() {
+  const [showFiles, setShowFiles] = useState(false)
+  const [selectedFileName, setSelectedFileName] = useState("")
+  const [file, setFile] = useState<File | null>(null)
+  const [classification, setClassification] = useState(null)
+  const [confidence, setConfidence] = useState(null)
+  const [trueLabel, setTrueLabel] = useState(null)
+
+  const csvFiles = [
+    "fraudulent_test_1.csv",
+    "legitimate_test_1.csv"
+  ]
+
+  const chooseCsvFile = async (fileName: string) => {
+    const response = await fetch(`/transactions/${fileName}`);
+
+    if (!response.ok) {
+      console.error("Failed to load CSV file.");
+      return;
+    }
+
+    const blob = await response.blob();
+
+    const selectedFile = new File([blob], fileName, {
+      type: "text/csv"
+    });
+
+    setFile(selectedFile);
+    setSelectedFileName(fileName);
+    setShowFiles(false);
+  }
+
+  const display_files = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setShowFiles(!showFiles);
+  }
+  
+  const predict = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    // Prevents the form from being submitted and page reloading first
+    event.preventDefault()
+
+    // Make sure a file has been selected
+    if (!file) {
+      console.error("File has not been selected.");
+      return;
+    }
+
+    // Append the file to formdata
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/predict",
+        {
+          method: "POST",
+          body: formData
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to make prediction.");
+      }
+
+      const data = await response.json();
+      setClassification(data.classification)
+      setConfidence(data.confidence)
+      setTrueLabel(data.true_label)
+    } catch (error) {
+      console.error("Failed to analyse the transaction: ", error);
+    }
+  }
+  
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    <main className="min-h-screen bg-stone-100 px-6 py-4">
+      <h1 className={`${garamond.className} text-8xl font-bold text-stone-900`}>Sentry</h1>
+      <div className="mx-auto max-w-3xl rounded-2xl bg-white mt-8 px-4 py-4">
+        <h2 className={`${garamond.className} text-5xl font-semibold mt-2 bg-stone-100`}>
+          Fraudulent Ethereum Transaction Detector
+        </h2>
+        <br></br>
+
+        <form className="space-y-4">
+          <button onClick={display_files} className="border border-stone-300 px-4 py-1 
+          mr-4 font-medium transition hover:bg-stone-100">
+            Choose transaction to make
+          </button>
+          <br></br>
+          {
+            showFiles && (
+              csvFiles.map((filename) => (
+                <button
+                  key={filename}
+                  type="button"
+                  onClick={() => chooseCsvFile(filename)}
+                  className="bg-stone-500 rounded-lg text-white font-medium transition
+                  hover:bg-stone-700 px-4 ml-2 mr-2">
+                  {filename}
+                </button>
+              ))
+            )
+          }
+          <br></br>
+          <button onClick={predict} className="border border-stone-300 px-4 py-1 mr-4 
+          font-medium transition hover:bg-stone-100">
+            Analyse transaction
+          </button>
+          <h3 className="mt-2 text-sm text-stone-600">
+            The model will output its decision regarding the transaction.
+          </h3>
+          {classification && 
+          <div className="mt-8 border-stone-200 bg-stone-50 font-bold">
+            {`Transaction is: ${classification}`}
+          </div>}
+          {confidence && <h3>{`Analysed with confidence: ${confidence}%`}</h3>}
+          {trueLabel && <h3>{`The transaction is actually: ${trueLabel}`}</h3>}
+        </form>
+
+        <h3 className="mt-8 text-sm text-stone-600">
+          The model has not seen these transactions previously, nor will it memorise these transactions.
+        </h3>
+      </div>
+    </main>
+  )
 }
